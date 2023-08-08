f0ac0e328e95dbc895b60543548e0510592c4b6b
Bram Adl <bram.adl@perioneer.com>
2023-08-08 22:15:14 +0700

    feat(home-page): set testimony section background image fixed

--- src/app/_sections/testimony-section.tsx
+++ src/app/_sections/testimony-section.tsx
@@ -10,7 +10,7 @@ export const TestimonySection: FC = () => {
       className="flex py-16 px-8 md:py-[100px] mx:px-[120px] flex-col justify-center items-center self-stretch"
       style={{
         background:
-          "linear-gradient(0deg, #2B2626B2, #2B2626B2), url(https://s3-alpha-sig.figma.com/img/9973/335a/86f95eda8c49629d1945db8e2b7ddc10?Expires=1692576000&Signature=AGJBV9vpncLLyIYrVobBWCXEbrwAsCsWmYz~fuTXaQImi~ed5F2k4VEZTMzJRCT9G9mlOWKKYS7xEAapnXXkc0edCtz0AcvdpsLDrT6k7TWVDEMDf~bR--Tt9F~G4-i5B42FF8cc5xnFMgRQemyNc4EgU4bKqw~LvKm8~Fi13ppY5uXKSoyl~82En1tBQxWR~P-mKDau3LvSVwYJS0eiA~wjRSl2xBZqYT~~xoXN8yCjrDk~UzTktxUwDzSVeQReHLHxf3pX41-Jd24gO68dBhipBhOFLJZBE~XXkL8S~Bh3UtV-DoomJHRGOBipm~4st~dyQ30jKZq7fZvBbQ1-fg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4) center center/cover",
+          "linear-gradient(0deg, #2B2626B2, #2B2626B2), url(https://s3-alpha-sig.figma.com/img/9973/335a/86f95eda8c49629d1945db8e2b7ddc10?Expires=1692576000&Signature=AGJBV9vpncLLyIYrVobBWCXEbrwAsCsWmYz~fuTXaQImi~ed5F2k4VEZTMzJRCT9G9mlOWKKYS7xEAapnXXkc0edCtz0AcvdpsLDrT6k7TWVDEMDf~bR--Tt9F~G4-i5B42FF8cc5xnFMgRQemyNc4EgU4bKqw~LvKm8~Fi13ppY5uXKSoyl~82En1tBQxWR~P-mKDau3LvSVwYJS0eiA~wjRSl2xBZqYT~~xoXN8yCjrDk~UzTktxUwDzSVeQReHLHxf3pX41-Jd24gO68dBhipBhOFLJZBE~XXkL8S~Bh3UtV-DoomJHRGOBipm~4st~dyQ30jKZq7fZvBbQ1-fg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4) fixed center/cover",
       }}
     >
       <div className="flex flex-col gap-8 text-primary-white md:max-w-[509px]">
