824c4217814d02159ca455e63e79b1cf81e4851b
Bram Adl <bram.adl@perioneer.com>
2023-08-08 12:01:51 +0700

    fix(fonts): use font variable instead of font classname

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,14 +15,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html className={montserratFont.className} lang="en">
+    <html className={montserratFont.variable} lang="en">
       <head>
         <link
           href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
           rel="stylesheet"
         />
       </head>
-      <body className={kollektifFont.className}>{children}</body>
+      <body className={kollektifFont.variable}>{children}</body>
     </html>
   );
 }
