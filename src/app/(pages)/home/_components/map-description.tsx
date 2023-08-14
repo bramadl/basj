914eeaf67beda176fd175906dda56c2c1caf27fb
Bram Adl <bram.adl@perioneer.com>
2023-08-14 10:14:51 +0700

    feat(home-page): apply fade in animation on map section

--- src/app/(pages)/home/_components/map-description.tsx
+++ src/app/(pages)/home/_components/map-description.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const MapDescription: FC = () => {
   return (
-    <p className="text-mobile-b1-400 md:text-desktop-b1-400">
+    <p className="map-description text-mobile-b1-400 md:text-desktop-b1-400">
       Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
       tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
       veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
--- src/app/(pages)/home/_sections/map-section.tsx
+++ src/app/(pages)/home/_sections/map-section.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import MapPNG from "@basj/assets/images/png/map.png";
+import { useFadeTransition } from "@basj/hooks/useFadeTransition";
 
 import Image from "next/image";
 import { FC } from "react";
@@ -7,8 +10,19 @@ import { MapTitle } from "../_components/map-title";
 import { MapDescription } from "../_components/map-description";
 
 export const MapSection: FC = () => {
+  const { scope } = useFadeTransition(
+    {
+      title: "h2",
+      description: ".map-description",
+      divider: ".divider",
+      image: ".map-image",
+    },
+    { staggerDelay: "-0.4", whenInView: true }
+  );
+
   return (
     <section
+      ref={scope}
       id="map"
       className="flex py-[72px] md:py-20 xl:py-[100px] px-8 md:px-[120px] xl:px-[160px] flex-col lg:flex-row items-start lg:items-center gap-[48px] md:gap-20 xl:gap-[72px] self-stretch bg-shades-gray-10"
     >
@@ -16,9 +30,9 @@ export const MapSection: FC = () => {
         <MapTitle />
         <MapDescription />
       </div>
-      <div className="border-t lg:border-r lg:border-t-0 border-shades-gray-40 self-stretch lg:self-center h-px lg:h-[365px] lg:w-px" />
+      <div className="divider border-t lg:border-r lg:border-t-0 border-shades-gray-40 self-stretch lg:self-center h-px lg:h-[365px] lg:w-px" />
       <div className="lg:flex-1 w-full lg:w-auto">
-        <Image alt="map" src={MapPNG} width={560} />
+        <Image alt="map" className="map-image" src={MapPNG} width={560} />
       </div>
     </section>
   );
