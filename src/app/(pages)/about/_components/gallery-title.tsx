163c347659b2c117a79015f84328ac065f186dd9
Bram Adl <bram.adl@perioneer.com>
2023-08-14 12:07:00 +0700

    feat(about-page): implement animation on gallery section

--- src/app/(pages)/about/_components/gallery-title.tsx
+++ src/app/(pages)/about/_components/gallery-title.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const GalleryTitle: FC = () => {
   return (
-    <h2 className="gallery-title self-stretch text-primary-dark text-center text-mobile-h2-700 lg:text-desktop-h3-700 xl:text-desktop-h2-700">
+    <h2 className="gallery-title self-stretch text-primary-dark text-center text-mobile-h2-700 lg:text-desktop-h3-700 xl:text-desktop-h2-700 opacity-0">
       Our people, our product, our process.
     </h2>
   );
