fd13d3dccc344fe8c8628a8d90b1e9a0d59e150d
Bram Adl <bram.adl@perioneer.com>
2023-08-14 12:05:56 +0700

    feat(about-page): implement the gallery section

---
+++ src/app/(pages)/about/_components/gallery-title.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+export const GalleryTitle: FC = () => {
+  return (
+    <h2 className="gallery-title self-stretch text-primary-dark text-center text-mobile-h2-700 lg:text-desktop-h3-700 xl:text-desktop-h2-700">
+      Our people, our product, our process.
+    </h2>
+  );
+};
--- src/app/(pages)/about/_sections/gallery-section.tsx
+++ src/app/(pages)/about/_sections/gallery-section.tsx
@@ -1,5 +1,15 @@
 import { FC } from "react";
+import { GalleryTitle } from "../_components/gallery-title";
+import { GalleryPhotos } from "../_components/gallery-photos";
 
 export const GallerySection: FC = () => {
-  return <div>GallerySection</div>;
+  return (
+    <section
+      id="gallery-section"
+      className="flex flex-col items-start gap-12 md:gap-20 xl:gap-[72px] self-stretch py-[72px] md:py-20 xl:py-[100px] px-8 md:px-[120px] xl:px-[160px] bg-primary-white"
+    >
+      <GalleryTitle />
+      <GalleryPhotos />
+    </section>
+  );
 };
