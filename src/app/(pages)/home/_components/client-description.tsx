878dc067994c175aaa1293771776ece450a59b39
Bram Adl <bram.adl@perioneer.com>
2023-08-14 10:12:24 +0700

    feat(home-page): apply fade in animation on client section

--- src/app/(pages)/home/_components/client-description.tsx
+++ src/app/(pages)/home/_components/client-description.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 
 export const ClientDescription: FC = () => {
   return (
-    <p className="text-mobile-b1-400 md:text-desktop-b1-400">
+    <p className="client-description text-mobile-b1-400 md:text-desktop-b1-400">
       PT Berkah Agung Semesta Jaya is committed to creating positive impacts on
       people and the planet. We are certified to meet high standards of social
       and environmental performance.
--- src/app/(pages)/home/_sections/client-section.tsx
+++ src/app/(pages)/home/_sections/client-section.tsx
@@ -1,3 +1,7 @@
+"use client";
+
+import { useFadeTransition } from "@basj/hooks/useFadeTransition";
+
 import { FC } from "react";
 
 import { ClientTitle } from "../_components/client-title";
@@ -5,8 +9,18 @@ import { ClientDescription } from "../_components/client-description";
 import { ClientList } from "../_components/client-list";
 
 export const ClientSection: FC = () => {
+  const { scope } = useFadeTransition(
+    {
+      title: "h2",
+      description: ".client-description",
+      images: ".client-list-image",
+    },
+    { staggerDelay: "-0.4", whenInView: true }
+  );
+
   return (
     <section
+      ref={scope}
       id="client"
       className="flex flex-col lg:flex-row items-start lg:items-center lg:justify-between gap-12 md:gap-20 xl:gap-[72px] self-stretch py-[72px] md:py-20 xl:py-[100px] px-8 md:px-[120px] xl:px-[160px] bg-shades-gray-10"
     >
